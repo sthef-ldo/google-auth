41180e210a6453b2ade28f942a16077e1756b61e
sthef <sthef.ldo@gmail.com>
2026-02-04 10:16:19 -0400

    funciona  ❀⸜(˶´ ˘ `˶)⸝❀ (
    se creo la autenficaciony guarda en la base de datos

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+
+Route::get('/google-auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+
+Route::get('/google-auth/callback', function () {
+    $user_google = Socialite::driver('google')->user();
+
+    // $user->token
+    $user = User::updateOrCreate(
+        [
+            'google_id' => $user_google->id,
+        ],
+        [
+            'name' => $user_google->name,
+            'email' => $user_google->email,
+        ]
+    );
+
+    Auth::login($user);
+    return redirect('/dashboard');
+});
+
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+
+
+
+
+require __DIR__ . '/settings.php';
